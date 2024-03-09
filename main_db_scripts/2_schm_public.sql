ffebfbb5918cadad033e6547de5d983ae6639469
sprittzer <tania.zvr@yandex.ru>
2024-03-09 23:04:08 +1000

    Delete main_db_scripts directory

--- main_db_scripts/2_schm_public.sql
+++
@@ -1,8 +0,0 @@
- -- удаляем у роли public права на схему public
-REVOKE ALL ON SCHEMA public FROM public;
---немножко оставляем
-GRANT usage ON SCHEMA public to public;
-GRANT usage ON SCHEMA pg_catalog to public;
-GRANT usage ON SCHEMA information_schema to public;
-GRANT EXECUTE ON all FUNCTIONs in schema public TO public;
-grant select on all tables in schema "public" to public;
